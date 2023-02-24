b5333257a8729ecbc2dcd5d161271f2cbc1f6181
sundipthapa <lakhanmgr123@gmail.com>
2023-02-25 00:18:17 +0545

    :lipstick: Matrix multiplication done

--- src/Question5/Solutionb.java
+++ src/Question5/Solutionb.java
@@ -1,5 +1,11 @@
 package Question5;
+/*
+Assume an electric vehicle must go from source city s to destination city d. You can locate many service centers along the journey that allow for the replacement of batteries; however, each service center provides batteries with a specific capacity. You are given a 2D array in which servicecenter[i]=[xi,yj] indicates that the ith service center is xi miles from the source city and offers yj miles after the automobile can travel after replacing batteries at specific service centers. Return the number of times the car's batteries need to be replaced before arriving at the destination.
+Input: serviceCenters = [{10,60},{20,30},{30,30},{60,40}], targetMiles= 100, startChargeCapacity = 10
+Output: 2
+Explanation: The car can go 10 miles on its initial capacity; after 10 miles, the car replaces batteries with a capacity of 60 miles; and after travelling 50 miles, at position 60 we change batteries with a capacity of 40 miles; and ultimately, we may arrive at our destination.
 
+ */
 class Solutionb {
     public int batteryReplacement(int[][] serviceCenters, int targetMiles, int startChargeCapacity) {
         // Initialize the number of battery replacements to 0
