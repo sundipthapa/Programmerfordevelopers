50604380df0ea8dcfbc632adbf4e023cdd068068
sundipthapa <lakhanmgr123@gmail.com>
2023-02-25 14:22:16 +0545

    :lipstick: question number 6a Huffman code

---
+++ src/Question6/Solutionb.java
@@ -0,0 +1,21 @@
+package Question6;
+/*
+b)	You are given an array of different words and target words. Each character of a word represents a different digit ranging from 0 to 9, and no two character are linked to same digit. If the sum of the numbers represented by each word on the array equals the sum the number represented by the targeted word, return true; otherwise, return false. Note: Provided list of words and targeted word is in the form of equation
+Input: words = ["SIX","SEVEN","SEVEN"], result = "TWENTY"
+Output: true
+Explanation:
+ s=6
+ I=5
+X=0,
+E=8,
+V=7,
+N=2,
+T=1,
+W=3,
+Y=4
+SIX +SEVEN + SEVEN = TWENTY
+650 + 68782 + 68782 = 138214
+
+ */
+public class Solutionb {
+}
